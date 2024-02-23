c20a61bdac5a1f8803814be583c5008070b976f6
AngryNxho <pleasetrixcl@gmail.com>
2024-02-23 04:41:05 -0300

    as

--- Redemption/roman.js
+++ Redemption/roman.js
@@ -1,5 +1,7 @@
 const Symbols = {
     I:1,
+    IV:4,
+    IX:9,
     V:5,
     X:10,
     L:50,
@@ -8,28 +10,41 @@ const Symbols = {
     M:1000,
 }
 
-let sum = 0;
-
+const Symbols2 = {
+    IV: 4,
+    IX: 9,
+}
 function check (arg) {
-    
-    for (let x of Object.entries(Symbols)) {
-        let i = 0;
-        for (const [key, value] of Object.entries(Symbols)) {
-            i++;
-            if (i > (String(arg).length) - 1) {
-                i = 0;
+    let sum = 0;
+    let sum2 = 0;
+    for (const [key, value] of Object.entries(Symbols)) {
+        // console.log(key, value);
+        for (let z of arg) {
+            if (key == z) {
+                sum += value;
             }
-            console.log(arg[i]);
         }
-        // console.log(i, arg.length);
-        // i++;
-        // if (i == arg.length) {
-        //     i = 0;
-        // }
+
     }
-    return '';    
+    
+    for (const [key2, value2] of Object.entries(Symbols2)) {
+        for (let ii in arg) {
+            for (let q = 1; q < (arg.length); q++) {
+                if (arg[ii] + arg[q] == key2) {
+                    console.log(arg[ii], arg[q]);
+                    sum2 += value2;
+                }
+                
+            }
+        }
+    }
+
+    return sum2;    
 }
 
-console.log(check("IVL"));
+// console.log(check("IIIV"));
+// console.log(check("LVIII"));
+// console.log(check("MCMXCIV"));
+console.log(check("IXIV"));
 
 
