84702fb1011c79e171662f6c3dd55b6313ea7c6c
AngryNxho <pleasetrixcl@gmail.com>
2024-03-06 20:09:57 -0300

    a

--- Redemption/roman.js
+++ Redemption/roman.js
@@ -1,45 +1,51 @@
 const Symbols = {
-    I:1,
-    IV:4,
-    IX:9,
-    V:5,
-    X:10,
-    L:50,
-    C:100,
-    D:500,
-    M:1000,
-}
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+  XL: 40,
+  XC: 90,
+  CD: 400,
+  CM: 900,
+  Z: 0,
+};
 
-// const Symbols2 = {
-    // IV: 4,
-    // IX: 9,
-// }
-
-function check (arg) {
-    let sum = 0;
-    let q = 0;
-    let y = 1;
-    for (const [key, value] of Object.entries(Symbols)) {
-        // console.log(key, value);
-        for (let z in arg) {
-            if (key == z) {
-                sum += value;
-            }
-            if (q < arg.length) {
-                q++; 
-                y++;
-            }
-        }
-    }
-
-
+var intToRoman = function (num) {
+  let sum = 0;
+  let sum2 = 0;
+  let y = 1;
  
-    return sum;    
-}
+  for (const [key, value] of Object.entries(Symbols)){
+    for (let z of num) {
+
+      if (z == key && z + num[y] != key) {
+        sum += value;
+      } else if (z + num[y] == key) {
+        console.log(key, value);
+        sum2 += value;
+      }
+     
+      y++;
+
+      if (y >= num.length) {
+        y = 0;
+      }
+    }
+  }
+
+  let result = sum - sum2;
+
+  
+  return `${sum} - ${sum2} ${result}`;
+};
 
 // console.log(check("IIIV"));
 // console.log(check("LVIII"));
-// console.log(check("MCMXCIV"));
-console.log(check("IXIV"));
-
+// console.log(intToRoman("MCMXCIV"));
+console.log(intToRoman("VI")); //9 4 10 5 1 = 29 correcto
 
