0cb9f58c72460cc1d480c452291ba380bc6a6484
AngryNxho <pleasetrixcl@gmail.com>
2024-03-12 02:53:20 -0300

    asf

--- python/toqueFama.py
+++ python/toqueFama.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-number_length = 3
+number_length = 9
 # int(input("Set the length of your code: "))
 secret_number = []
 
@@ -14,23 +14,34 @@ def secret_code():
         current_number = random_number() 
         if (current_number not in secret_number):
             secret_number.append(current_number)
-    return str(secret_number)
+    return secret_number
 
 def user():
     lifes = number_length
     toques = 0
+    famas = 0
     save_secret = secret_code()
-    print(save_secret)
+    y=1
     while lifes != 0:
-        user_guess = input("Ingresa un numero: ")
+        user_guess ="123406789" 
+        #input("Ingresa un numero: ")
         lifes-=1
         for x in user_guess:
-            if (x in save_secret):
+            if (x in str(save_secret)):
                 toques += 1
-        print(f"Toques: {toques}")
-        toques = 0
 
-    return "hola"
+            else:
+                for z in range(len(user_guess)):
+                    for i in (save_secret):
+                        if (user_guess[z] == str(i)):
+                            famas += 1
+
+                        
+        print(f"Toques: {toques} Famas: {famas}")
+        toques = 0
+        famas = 0
+
+    return f"user {user_guess} code  {save_secret}"
 
 print(user())
     
