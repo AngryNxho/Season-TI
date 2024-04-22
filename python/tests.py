7072e4931b8a879c1f82447c242998d7ad4c9789
AngryNxho <pleasetrixcl@gmail.com>
2024-04-22 16:29:34 -0400

    semana7

--- python/tests.py
+++ python/tests.py
@@ -1,13 +1,6 @@
-registros = [["654321", 2012, 'p', 'volvo', 'xd'], 
-    ["123456", 2012, 'p', 'volvo', 'xd'],
-    ["987654", 2010, 'p', 'toyota', 'lol'], 
-    ["456789", 2010, 'p', 'toyota', 'lol'],
-    ["333444", 2015, 'p', 'ford', 'haha'], 
-    ["555666", 2015, 'p', 'ford', 'haha'],
-    ["111222", 2018, 'p', 'chevrolet', 'rofl'], 
-    ["999888", 2018, 'p', 'chevrolet', 'rofl'],
-    ["777888", 2020, 'p', 'honda', 'lmao'], 
-    ["222333", 2020, 'p', 'honda', 'lmao']
-]
+import numpy as np
 
+matriz = np.array([[1,2,3],[4,5,1],[2,2,1]])
+
+print(f"El resultado de la suma es: {np.sum(matriz)}")
 
